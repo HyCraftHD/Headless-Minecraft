575cbca8520ea33e812b968c539aa671ee11a948
HyCraftHD <hycrafthd@live.de>
2021-03-22 18:49:36 +0100

    Basic main setup

--- src/main/java/net/hycrafthd/headless_minecraft/Main.java
+++ src/main/java/net/hycrafthd/headless_minecraft/Main.java
@@ -1,9 +1,16 @@
 package net.hycrafthd.headless_minecraft;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import net.hycrafthd.logging.LoggingUtil;
+
 public class Main {
 	
+	public static final Logger LOGGER = LogManager.getLogger("Headless Minecraft");
+	
 	public static void main(String[] args) {
-		
+		LoggingUtil.redirectPrintStreams(LOGGER);
 	}
 	
 }
