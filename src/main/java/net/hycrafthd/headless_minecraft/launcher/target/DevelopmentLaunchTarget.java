820e2303c9f26021efa6a3861f5e622ff24fc97c
HyCraftHD <hycrafthd@live.de>
2021-03-26 16:09:55 +0100

    Add jars to the transforming class loader

--- src/main/java/net/hycrafthd/headless_minecraft/launcher/target/DevelopmentLaunchTarget.java
+++ src/main/java/net/hycrafthd/headless_minecraft/launcher/target/DevelopmentLaunchTarget.java
@@ -1,10 +1,16 @@
 package net.hycrafthd.headless_minecraft.launcher.target;
 
+import java.nio.file.Paths;
 import java.util.concurrent.Callable;
 
 import cpw.mods.modlauncher.api.ILaunchHandlerService;
 import cpw.mods.modlauncher.api.ITransformingClassLoader;
 import cpw.mods.modlauncher.api.ITransformingClassLoaderBuilder;
+import net.hycrafthd.headless_minecraft.launcher.Constants;
+import net.hycrafthd.headless_minecraft.launcher.MinecraftSetup;
+import net.hycrafthd.minecraft_downloader.library.DownloadableFile;
+import net.hycrafthd.minecraft_downloader.settings.GeneratedSettings;
+import net.hycrafthd.minecraft_downloader.settings.ProvidedSettings;
 
 public class DevelopmentLaunchTarget implements ILaunchHandlerService {
 	
@@ -15,11 +21,24 @@ public class DevelopmentLaunchTarget implements ILaunchHandlerService {
 	
 	@Override
 	public void configureTransformationClassLoader(ITransformingClassLoaderBuilder builder) {
+		final ProvidedSettings settings = MinecraftSetup.getInstance().getSettings();
+		final GeneratedSettings generatedSettings = settings.getGeneratedSettings();
 		
+		// Add all libraries to the transforming path
+		generatedSettings.getDownloadableFiles() //
+				.stream() //
+				.filter(downloadableFile -> !downloadableFile.isNative()) //
+				.filter(DownloadableFile::hasDownloadedFile) //
+				.map(DownloadableFile::getDownloadedFile) //
+				.forEach(file -> builder.addTransformationPath(file.toPath()));
+		
+		// Add mapped minecraft jar
+		builder.addTransformationPath(Paths.get(Constants.DEVELOPMENT_MAPPED_MINECRAFT));
 	}
 	
 	@Override
 	public Callable<Void> launchService(String[] arguments, ITransformingClassLoader launchClassLoader) {
+		MinecraftSetup.destroy();
 		return () -> {
 			return null;
 		};
