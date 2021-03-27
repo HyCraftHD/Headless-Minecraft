830d668ced7458adc2a0673fb41e12a82b46fc0b
HyCraftHD <hycrafthd@live.de>
2021-03-27 03:44:10 +0100

    Add basic main

--- headless-minecraft/src/main/java/net/hycrafthd/headless_minecraft/Main.java
+++ headless-minecraft/src/main/java/net/hycrafthd/headless_minecraft/Main.java
@@ -1,16 +1,40 @@
 package net.hycrafthd.headless_minecraft;
 
+import java.io.File;
+import java.io.IOException;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.spongepowered.asm.mixin.Mixins;
+
+import joptsimple.OptionParser;
+import joptsimple.OptionSet;
+import joptsimple.OptionSpec;
 
 public class Main {
 	
 	public static final Logger LOGGER = LogManager.getLogger("Headless Minecraft");
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		Mixins.addConfiguration("mixin.json");
 		
+		final OptionParser parser = new OptionParser();
+		
+		// Default specs
+		final OptionSpec<Void> helpSpec = parser.accepts("help", "Show the help menu").forHelp();
+		final OptionSpec<File> runSpec = parser.accepts("run", "Run directory for headless minecraft").withRequiredArg().ofType(File.class);
+		
+		// User data specs
+		final OptionSpec<String> authNameSpec = parser.accepts("auth-name", "Player name").withRequiredArg();
+		final OptionSpec<String> authUuidSpec = parser.accepts("auth-uuid", "Player uuid").withRequiredArg();
+		final OptionSpec<String> authTokenSpec = parser.accepts("auth-token", "Auth / Access token").withRequiredArg();
+		
+		final OptionSet set = parser.parse(args);
+		
+		if (set.has(helpSpec) || set.specs().size() < 4) {
+			parser.printHelpOn(System.out);
+			return;
+		}
 	}
 	
 }
