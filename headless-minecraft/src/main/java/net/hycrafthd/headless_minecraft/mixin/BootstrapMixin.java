a11e93f0963f89e651763962a1b32e97448edf44
HyCraftHD <hycrafthd@live.de>
2021-03-28 13:45:16 +0200

    Remove wrapStreams

---
+++ headless-minecraft/src/main/java/net/hycrafthd/headless_minecraft/mixin/BootstrapMixin.java
@@ -0,0 +1,18 @@
+package net.hycrafthd.headless_minecraft.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.minecraft.server.Bootstrap;
+
+@Mixin(Bootstrap.class)
+public class BootstrapMixin {
+	
+	@Inject(method = "wrapStreams", cancellable = true, at = @At("HEAD"))
+	private static void removeStreamWrapping(CallbackInfo info) {
+		info.cancel();
+	}
+	
+}
