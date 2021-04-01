79a338b9af1357c156d85c4c3280555cc94b5de4
HyCraftHD <hycrafthd@live.de>
2021-04-01 23:15:14 +0200

    Remove tint cache methods

--- headless-minecraft/src/main/java/net/hycrafthd/headless_minecraft/mixin/ClientLevelMixin.java
+++ headless-minecraft/src/main/java/net/hycrafthd/headless_minecraft/mixin/ClientLevelMixin.java
@@ -3,6 +3,7 @@ package net.hycrafthd.headless_minecraft.mixin;
 import java.util.function.Consumer;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
@@ -17,9 +18,18 @@ abstract class ClientLevelMixin {
 		return null;
 	}
 	
+	// Remove tintCaches
+	
 	@Redirect(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/Util;make(Ljava/lang/Object;Ljava/util/function/Consumer;)Ljava/lang/Object;"))
 	private Object returnNullForUtilMake(Object object, Consumer<?> consumer) {
 		return null;
 	}
 	
+	@Overwrite
+	public void onChunkLoaded(int x, int z) {
+	}
+	
+	@Overwrite
+	public void clearTintCaches() {
+	}
 }
