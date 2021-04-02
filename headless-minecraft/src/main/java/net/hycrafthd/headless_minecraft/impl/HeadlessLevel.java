5161aba6dd84bfe9a6e856f6150f0b3b1152a3ef
HyCraftHD <hycrafthd@live.de>
2021-04-02 15:31:47 +0200

    Override most client only methods that are removed + fix crashreports and some other stuff (wip, some stuff still not fixed)

--- headless-minecraft/src/main/java/net/hycrafthd/headless_minecraft/impl/HeadlessLevel.java
+++ headless-minecraft/src/main/java/net/hycrafthd/headless_minecraft/impl/HeadlessLevel.java
@@ -1,12 +1,25 @@
 package net.hycrafthd.headless_minecraft.impl;
 
+import java.util.Random;
 import java.util.function.Supplier;
 
 import net.hycrafthd.headless_minecraft.network.ClientListener;
+import net.minecraft.CrashReport;
+import net.minecraft.CrashReportCategory;
 import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.BlockPos.MutableBlockPos;
+import net.minecraft.core.particles.ParticleOptions;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.resources.ResourceKey;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.util.profiling.ProfilerFiller;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.ColorResolver;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.dimension.DimensionType;
 
 public class HeadlessLevel extends ClientLevel {
@@ -18,4 +31,105 @@ public class HeadlessLevel extends ClientLevel {
 		this.clientListener = clientListener;
 	}
 	
+	@Override
+	public String toString() {
+		return "HeadlessLevel";
+	}
+	
+	@Override
+	public CrashReportCategory fillReportDetails(CrashReport crashReport) {
+		final CrashReportCategory category = super.fillReportDetails(crashReport);
+		category.setDetail("Headless Level", () -> {
+			return "Yes";
+		});
+		return category;
+	}
+	
+	// Override some unused methods
+	
+	@Override
+	public void onChunkLoaded(int x, int z) {
+	}
+	
+	@Override
+	public void clearTintCaches() {
+	}
+	
+	@Override
+	public void animateTick(int x, int y, int z) {
+	}
+	
+	@Override
+	public void doAnimateTick(int x, int y, int z, int distance, Random random, boolean barrier, MutableBlockPos pos) {
+	}
+	
+	@Override
+	public void playSound(Player player, double x, double y, double z, SoundEvent event, SoundSource source, float volume, float pitch) {
+	}
+	
+	@Override
+	public void playSound(Player player, Entity entity, SoundEvent event, SoundSource source, float volume, float pitch) {
+	}
+	
+	@Override
+	public void playLocalSound(BlockPos pos, SoundEvent event, SoundSource source, float volume, float pitch, boolean delay) {
+	}
+	
+	@Override
+	public void playLocalSound(double x, double y, double z, SoundEvent event, SoundSource source, float volume, float pitch, boolean delay) {
+	}
+	
+	@Override
+	public void createFireworks(double x, double y, double z, double motionX, double motionY, double motionZ, CompoundTag compound) {
+	}
+	
+	@Override
+	public void sendBlockUpdated(BlockPos pos, BlockState oldState, BlockState newState, int flags) {
+	}
+	
+	@Override
+	public void setBlocksDirty(BlockPos pos, BlockState oldState, BlockState newState) {
+	}
+	
+	@Override
+	public void setSectionDirtyWithNeighbors(int x, int y, int z) {
+	}
+	
+	@Override
+	public void destroyBlockProgress(int breaker, BlockPos pos, int progress) {
+	}
+	
+	@Override
+	public void globalLevelEvent(int type, BlockPos pos, int data) {
+	}
+	
+	@Override
+	public void levelEvent(Player player, int type, BlockPos pos, int data) {
+	}
+	
+	@Override
+	public void addParticle(ParticleOptions options, double x, double y, double z, double speedX, double speedY, double speedZ) {
+	}
+	
+	@Override
+	public void addParticle(ParticleOptions options, boolean alwaysVisible, double x, double y, double z, double speedX, double speedY, double speedZ) {
+	}
+	
+	@Override
+	public void addAlwaysVisibleParticle(ParticleOptions options, double x, double y, double z, double speedX, double speedY, double speedZ) {
+	}
+	
+	@Override
+	public void addAlwaysVisibleParticle(ParticleOptions options, boolean ignoreRange, double x, double y, double z, double speedX, double speedY, double speedZ) {
+	}
+	
+	@Override
+	public int getBlockTint(BlockPos pos, ColorResolver resolver) {
+		return 0;
+	}
+	
+	@Override
+	public int calculateBlockTint(BlockPos pos, ColorResolver resolver) {
+		return 0;
+	}
 }
--- headless-minecraft/src/main/java/net/hycrafthd/headless_minecraft/mixin/ClientLevelMixin.java
+++ headless-minecraft/src/main/java/net/hycrafthd/headless_minecraft/mixin/ClientLevelMixin.java
@@ -3,33 +3,42 @@ package net.hycrafthd.headless_minecraft.mixin;
 import java.util.function.Consumer;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
+import net.minecraft.CrashReport;
+import net.minecraft.CrashReportCategory;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ClientLevel;
 
 @Mixin(ClientLevel.class)
-abstract class ClientLevelMixin {
+public abstract class ClientLevelMixin {
+	
+	// Remove / replace minecraft instances
 	
 	@Redirect(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/Minecraft;getInstance()Lnet/minecraft/client/Minecraft;"))
 	private Minecraft returnNullForMinecraftInstance() {
 		return null;
 	}
 	
-	// Remove tintCaches
-	
 	@Redirect(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/Util;make(Ljava/lang/Object;Ljava/util/function/Consumer;)Ljava/lang/Object;"))
 	private Object returnNullForUtilMake(Object object, Consumer<?> consumer) {
 		return null;
 	}
 	
-	@Overwrite
-	public void onChunkLoaded(int x, int z) {
+	@Inject(method = "fillReportDetails", cancellable = true, locals = LocalCapture.CAPTURE_FAILHARD, at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/world/level/Level;fillReportDetails(Lnet/minecraft/CrashReport;)Lnet/minecraft/CrashReportCategory;"))
+	public void replaceCrashReportDetails(CrashReport report, CallbackInfoReturnable<CrashReportCategory> callback, CrashReportCategory category) {
+		category.setDetail("Server brand", () -> {
+			return "UNKNOWN YET"; // TODO
+		});
+		category.setDetail("Server type", () -> {
+			return "Multiplayer";
+		});
+		callback.setReturnValue(category);
+		callback.cancel();
 	}
 	
-	@Overwrite
-	public void clearTintCaches() {
-	}
 }
