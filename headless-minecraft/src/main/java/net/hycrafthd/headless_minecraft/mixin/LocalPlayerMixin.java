03ad553ba122431e263c063864afc73feb9593df
LucZZ <lucas.weiss1999@web.de>
2021-04-02 20:25:38 +0200

    Mixin for LocalPlayer

---
+++ headless-minecraft/src/main/java/net/hycrafthd/headless_minecraft/mixin/LocalPlayerMixin.java
@@ -0,0 +1,10 @@
+package net.hycrafthd.headless_minecraft.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+
+import net.minecraft.client.player.LocalPlayer;
+
+@Mixin(LocalPlayer.class)
+public abstract class LocalPlayerMixin {
+	
+}
