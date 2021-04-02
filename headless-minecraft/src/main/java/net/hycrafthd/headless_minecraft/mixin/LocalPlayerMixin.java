ae77a3a351e57dbda0f98e36da31eb2e0c347642
LucZZ <lucas.weiss1999@web.de>
2021-04-02 21:03:20 +0200

    Remove getSoundManager call with mixin (return null)

--- headless-minecraft/src/main/java/net/hycrafthd/headless_minecraft/mixin/LocalPlayerMixin.java
+++ headless-minecraft/src/main/java/net/hycrafthd/headless_minecraft/mixin/LocalPlayerMixin.java
@@ -1,10 +1,19 @@
 package net.hycrafthd.headless_minecraft.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.client.sounds.SoundManager;
 
 @Mixin(LocalPlayer.class)
 public abstract class LocalPlayerMixin {
+	
+	@Redirect(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/Minecraft;getSoundManager()Lnet/minecraft/client/sounds/SoundManager;"))
+	private SoundManager replaceTest(Minecraft minecraft) {
+		return null;
+	}
 	
 }
