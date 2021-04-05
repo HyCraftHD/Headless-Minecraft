c1103afc6c165bd55d10450d2dab01e0b199ddda
HyCraftHD <hycrafthd@live.de>
2021-04-05 15:16:24 +0200

    Replace getUser with mixin

--- headless-minecraft/src/main/java/net/hycrafthd/headless_minecraft/mixin/ClientHandshakePacketListenerImplMixin.java
+++ headless-minecraft/src/main/java/net/hycrafthd/headless_minecraft/mixin/ClientHandshakePacketListenerImplMixin.java
@@ -3,8 +3,16 @@ package net.hycrafthd.headless_minecraft.mixin;
 import java.util.function.Consumer;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.gen.Accessor;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
+import com.mojang.authlib.minecraft.MinecraftSessionService;
+
+import net.hycrafthd.headless_minecraft.HeadlessMinecraft;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.User;
 import net.minecraft.client.multiplayer.ClientHandshakePacketListenerImpl;
 import net.minecraft.network.chat.Component;
 
@@ -13,5 +21,15 @@ public abstract class ClientHandshakePacketListenerImplMixin {
 	
 	@Accessor("updateStatus")
 	public abstract Consumer<Component> getUpdateStatus();
+	
+	@Redirect(method = "authenticateServer", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/Minecraft;getUser()Lnet/minecraft/client/User;"))
+	private User getHeadlessUser(Minecraft minecraft) {
+		return HeadlessMinecraft.getInstance().getUser();
+	}
+	
+	@Overwrite(aliases = "getMinecraftSessionService")
+	private MinecraftSessionService getMinecraftSessionService() {
+		return HeadlessMinecraft.getInstance().getSessionService();
+	}
 	
 }
