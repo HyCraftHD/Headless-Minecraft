424f7ba5fe335155e8655ead6a4fc5aed377ed85
HyCraftHD <hycrafthd@live.de>
2021-04-05 16:19:08 +0200

    Update connect to server

--- headless-minecraft/src/main/java/net/hycrafthd/headless_minecraft/network/ConnectionManager.java
+++ headless-minecraft/src/main/java/net/hycrafthd/headless_minecraft/network/ConnectionManager.java
@@ -1,31 +1,60 @@
 package net.hycrafthd.headless_minecraft.network;
 
+import java.net.InetAddress;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import net.hycrafthd.headless_minecraft.HeadlessMinecraft;
+import net.minecraft.client.multiplayer.ServerAddress;
 import net.minecraft.client.multiplayer.ServerData;
 import net.minecraft.network.Connection;
+import net.minecraft.network.ConnectionProtocol;
+import net.minecraft.network.protocol.handshake.ClientIntentionPacket;
+import net.minecraft.network.protocol.login.ServerboundHelloPacket;
 
 public class ConnectionManager {
 	
+	public static final Logger LOGGER = LogManager.getLogger();
+	
+	private final HeadlessMinecraft headlessMinecraft;
+	
 	private ServerData serverData;
 	private Connection connection;
+	
+	public ConnectionManager(HeadlessMinecraft headlessMinecraft) {
+		this.headlessMinecraft = headlessMinecraft;
+	}
+	
+	public void tick() {
+		if (connection != null) {
+			connection.tick();
+		}
+	}
+	
+	public void connectToServer() {
+		final ServerAddress serverAddress = ServerAddress.parseString(serverData.ip);
+		
+		LOGGER.info("Try to connect to " + serverAddress.getHost() + ":" + serverAddress.getPort());
+		
+		try {
+			connection = Connection.connectToServer(InetAddress.getByName(serverAddress.getHost()), serverAddress.getPort(), true);
+			connection.setListener(new HeadlessLoginPacketListener(headlessMinecraft, connection, component -> LOGGER.info("Connection state: {}", component.getString())));
+			
+			LOGGER.info("Connection initiated, start handshake");
+			
+			connection.send(new ClientIntentionPacket(serverAddress.getHost(), serverAddress.getPort(), ConnectionProtocol.LOGIN));
+			connection.send(new ServerboundHelloPacket(headlessMinecraft.getUser().getGameProfile()));
+		} catch (final Exception ex) {
+			LOGGER.error("Could not connect to server", ex);
+		}
+	}
 	
 	public ServerData getServerData() {
 		return serverData;
 	}
 	
-	public void setServerData(ServerData serverData) {
-		this.serverData = serverData;
-	}
-	
 	public Connection getConnection() {
 		return connection;
 	}
-	
-	public void setConnection(Connection connection) {
-		this.connection = connection;
-	}
-	
-	public void tick() {
-		
-	}
-	
 }
