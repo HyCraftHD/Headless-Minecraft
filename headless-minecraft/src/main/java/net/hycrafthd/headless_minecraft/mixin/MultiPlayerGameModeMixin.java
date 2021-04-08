c3f93f7a730202a0287959939e69926034287490
HyCraftHD <hycrafthd@live.de>
2021-04-08 21:14:21 +0200

    Add multiplayer gamemode mixin

---
+++ headless-minecraft/src/main/java/net/hycrafthd/headless_minecraft/mixin/MultiPlayerGameModeMixin.java
@@ -0,0 +1,10 @@
+package net.hycrafthd.headless_minecraft.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+
+import net.minecraft.client.multiplayer.MultiPlayerGameMode;
+
+@Mixin(MultiPlayerGameMode.class)
+public abstract class MultiPlayerGameModeMixin {
+	
+}
