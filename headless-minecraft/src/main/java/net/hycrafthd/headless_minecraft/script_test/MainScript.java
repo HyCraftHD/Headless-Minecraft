751fbc3dbc5ce3131b366372e1277603673b5ce8
HyCraftHD <hycrafthd@live.de>
2021-04-08 13:53:28 +0200

    Add test in test script

--- headless-minecraft/src/main/java/net/hycrafthd/headless_minecraft/script_test/MainScript.java
+++ headless-minecraft/src/main/java/net/hycrafthd/headless_minecraft/script_test/MainScript.java
@@ -3,8 +3,15 @@ package net.hycrafthd.headless_minecraft.script_test;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import net.hycrafthd.headless_minecraft.HeadlessMinecraft;
+import net.hycrafthd.headless_minecraft.network.ConnectionManager;
 import net.hycrafthd.headless_minecraft.script.IScript;
 
+/**
+ * This is just a test script to test some stuff.
+ * 
+ * @author HyCraftHD
+ */
 public class MainScript implements IScript {
 	
 	private static final Logger LOGGER = LogManager.getLogger();
@@ -21,6 +28,10 @@ public class MainScript implements IScript {
 	@Override
 	public void enable() {
 		LOGGER.info("Finished loading main script.");
+		final ConnectionManager connectionManager = HeadlessMinecraft.getInstance().getConnectionManager();
+		
+		connectionManager.updateServerData("localhost");
+		connectionManager.connectToServer();
 	}
 	
 }
