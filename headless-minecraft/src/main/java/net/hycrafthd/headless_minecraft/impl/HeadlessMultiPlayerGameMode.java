00437717bdff435e97fbd14c2c9aab55c05643cc
HyCraftHD <hycrafthd@live.de>
2021-04-08 22:38:47 +0200

    Return null in local player as it should not be used

--- headless-minecraft/src/main/java/net/hycrafthd/headless_minecraft/impl/HeadlessMultiPlayerGameMode.java
+++ headless-minecraft/src/main/java/net/hycrafthd/headless_minecraft/impl/HeadlessMultiPlayerGameMode.java
@@ -1,12 +1,21 @@
 package net.hycrafthd.headless_minecraft.impl;
 
 import net.hycrafthd.headless_minecraft.network.HeadlessPacketListener;
+import net.minecraft.client.ClientRecipeBook;
+import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.multiplayer.MultiPlayerGameMode;
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.stats.StatsCounter;
 
 public class HeadlessMultiPlayerGameMode extends MultiPlayerGameMode {
 	
 	public HeadlessMultiPlayerGameMode(HeadlessPacketListener listener) {
 		super(null, listener);
+	}
+	
+	@Override
+	public LocalPlayer createPlayer(ClientLevel name, StatsCounter name2, ClientRecipeBook name3, boolean name4, boolean name5) {
+		return null;
 	}
 	
 }
