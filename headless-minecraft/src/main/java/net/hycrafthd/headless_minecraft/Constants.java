f00efe056706058ed0dfdc80ed2d31298be952ca
HyCraftHD <hycrafthd@live.de>
2021-04-08 13:25:49 +0200

    Print chat to logger

--- headless-minecraft/src/main/java/net/hycrafthd/headless_minecraft/Constants.java
+++ headless-minecraft/src/main/java/net/hycrafthd/headless_minecraft/Constants.java
@@ -1,7 +1,12 @@
 package net.hycrafthd.headless_minecraft;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Constants {
 	
 	public static final String NAME = "Headless Client";
+	
+	public static final Logger CHAT_LOGGER = LogManager.getLogger("Chat");
 	
 }
