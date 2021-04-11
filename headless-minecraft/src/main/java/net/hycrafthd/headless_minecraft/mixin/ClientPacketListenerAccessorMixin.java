b8dc54197f96e0647d2648c46ac5848fd6d3f798
HyCraftHD <hycrafthd@live.de>
2021-04-11 14:50:24 +0200

    Add more accessors

--- headless-minecraft/src/main/java/net/hycrafthd/headless_minecraft/mixin/ClientPacketListenerAccessorMixin.java
+++ headless-minecraft/src/main/java/net/hycrafthd/headless_minecraft/mixin/ClientPacketListenerAccessorMixin.java
@@ -1,11 +1,16 @@
 package net.hycrafthd.headless_minecraft.mixin;
 
+import java.util.Set;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
 import net.minecraft.client.multiplayer.ClientAdvancements;
 import net.minecraft.client.multiplayer.ClientPacketListener;
 import net.minecraft.client.multiplayer.ClientSuggestionProvider;
+import net.minecraft.core.RegistryAccess;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.level.Level;
 
 @Mixin(ClientPacketListener.class)
 public interface ClientPacketListenerAccessorMixin {
@@ -15,5 +20,11 @@ public interface ClientPacketListenerAccessorMixin {
 	
 	@Accessor("suggestionsProvider")
 	public void setSuggestionsProvider(ClientSuggestionProvider suggestionsProvider);
+	
+	@Accessor("levels")
+	public void setLevels(Set<ResourceKey<Level>> levels);
+	
+	@Accessor("registryAccess")
+	public void setRegistryAccess(RegistryAccess registryAccess);
 	
 }
