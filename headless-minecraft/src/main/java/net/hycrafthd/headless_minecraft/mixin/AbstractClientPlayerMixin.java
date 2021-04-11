02fa3d7947fc8cb82e9ea96677778d650f652be8
HyCraftHD <hycrafthd@live.de>
2021-04-11 19:57:51 +0200

    Add abstract client player mixin

---
+++ headless-minecraft/src/main/java/net/hycrafthd/headless_minecraft/mixin/AbstractClientPlayerMixin.java
@@ -0,0 +1,10 @@
+package net.hycrafthd.headless_minecraft.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+
+import net.minecraft.client.player.AbstractClientPlayer;
+
+@Mixin(AbstractClientPlayer.class)
+public abstract class AbstractClientPlayerMixin {
+	
+}
