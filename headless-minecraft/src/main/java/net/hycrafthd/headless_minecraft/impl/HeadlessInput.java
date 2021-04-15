0ceae95dc30df50448523da540a7cf06e6bc2257
HyCraftHD <hycrafthd@live.de>
2021-04-15 15:27:23 +0200

    Add headless input

---
+++ headless-minecraft/src/main/java/net/hycrafthd/headless_minecraft/impl/HeadlessInput.java
@@ -0,0 +1,12 @@
+package net.hycrafthd.headless_minecraft.impl;
+
+import net.minecraft.client.player.Input;
+
+public class HeadlessInput extends Input {
+	
+	@Override
+	public void tick(boolean movingSlowly) {
+		super.tick(movingSlowly);
+	}
+	
+}
