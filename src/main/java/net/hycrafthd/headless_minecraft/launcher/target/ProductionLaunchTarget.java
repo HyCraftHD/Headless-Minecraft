eae806b88dde71ab559dd3b183056a22d386fb10
HyCraftHD <hycrafthd@live.de>
2021-04-18 00:03:50 +0200

    Add minecraft jar

--- src/main/java/net/hycrafthd/headless_minecraft/launcher/target/ProductionLaunchTarget.java
+++ src/main/java/net/hycrafthd/headless_minecraft/launcher/target/ProductionLaunchTarget.java
@@ -1,6 +1,8 @@
 package net.hycrafthd.headless_minecraft.launcher.target;
 
 import cpw.mods.modlauncher.api.ITransformingClassLoaderBuilder;
+import net.hycrafthd.headless_minecraft.launcher.setup.MinecraftSetup;
+import net.hycrafthd.minecraft_downloader.settings.ProvidedSettings;
 
 public class ProductionLaunchTarget extends BaseLaunchTarget {
 	
@@ -11,6 +13,9 @@ public class ProductionLaunchTarget extends BaseLaunchTarget {
 	
 	@Override
 	protected void configureTargetSpecificTransformationClassLoader(ITransformingClassLoaderBuilder builder) {
+		final ProvidedSettings settings = MinecraftSetup.getInstance().getSettings();
 		
+		// Add minecraft jar
+		builder.addTransformationPath(settings.getClientJarFile().toPath());
 	}
 }
