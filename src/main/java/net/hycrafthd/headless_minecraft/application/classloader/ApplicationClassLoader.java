0a860eb6e0b36fadd45a3834efb43146307bf126
HyCraftHD <hycrafthd@live.de>
2021-04-20 03:13:31 +0200

    Add basic application stuff

---
+++ src/main/java/net/hycrafthd/headless_minecraft/application/classloader/ApplicationClassLoader.java
@@ -0,0 +1,16 @@
+package net.hycrafthd.headless_minecraft.application.classloader;
+
+import java.net.URL;
+import java.net.URLClassLoader;
+
+public class ApplicationClassLoader extends URLClassLoader {
+	
+	static {
+		ClassLoader.registerAsParallelCapable();
+	}
+	
+	public ApplicationClassLoader() {
+		super(new URL[] {}, ApplicationClassLoader.class.getClassLoader());
+	}
+	
+}
