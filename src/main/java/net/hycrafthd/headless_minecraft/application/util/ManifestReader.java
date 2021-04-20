ef6c5812e713fc4e8de1b48d8f565dcfc2fb7a2b
HyCraftHD <hycrafthd@live.de>
2021-04-20 03:38:33 +0200

    Add manifest reader

---
+++ src/main/java/net/hycrafthd/headless_minecraft/application/util/ManifestReader.java
@@ -0,0 +1,21 @@
+package net.hycrafthd.headless_minecraft.application.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.jar.JarFile;
+import java.util.jar.Manifest;
+
+public class ManifestReader {
+	
+	public static Manifest readManifest(Class<?> clazz) {
+		final URL url = clazz.getResource("/" + JarFile.MANIFEST_NAME);
+		
+		try (final InputStream inputStream = url.openStream()) {
+			return new Manifest(inputStream);
+		} catch (final IOException ex) {
+			throw new IllegalStateException("Cannot read manifest", ex);
+		}
+	}
+	
+}
