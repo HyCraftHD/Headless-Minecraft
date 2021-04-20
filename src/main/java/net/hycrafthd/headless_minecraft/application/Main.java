5b1e191a5c370a287d8eedb29f7a1f35d389af5b
HyCraftHD <hycrafthd@live.de>
2021-04-20 03:05:55 +0200

    Move classes a bit

--- src/main/java/net/hycrafthd/headless_minecraft/application/Main.java
+++ src/main/java/net/hycrafthd/headless_minecraft/application/Main.java
@@ -2,6 +2,8 @@ package net.hycrafthd.headless_minecraft.application;
 
 import java.net.URL;
 
+import net.hycrafthd.headless_minecraft.application.util.URLStreamHandlerClassPath;
+
 public class Main {
 	
 	public static void main(String[] args) {
