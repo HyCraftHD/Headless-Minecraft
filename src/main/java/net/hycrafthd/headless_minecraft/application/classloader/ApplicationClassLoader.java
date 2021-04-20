7a5601d42d8ebe59c0d9f30a50f8bc04ca6df95f
HyCraftHD <hycrafthd@live.de>
2021-04-20 21:31:01 +0200

    Fix inheritance for classloader

--- src/main/java/net/hycrafthd/headless_minecraft/application/classloader/ApplicationClassLoader.java
+++ src/main/java/net/hycrafthd/headless_minecraft/application/classloader/ApplicationClassLoader.java
@@ -1,5 +1,6 @@
 package net.hycrafthd.headless_minecraft.application.classloader;
 
+import java.lang.reflect.Method;
 import java.net.URL;
 import java.net.URLClassLoader;
 
@@ -10,7 +11,7 @@ public class ApplicationClassLoader extends URLClassLoader {
 	}
 	
 	public ApplicationClassLoader() {
-		super(new URL[0]);
+		super(new URL[0], getPlatformClassLoader());
 	}
 	
 	@Override
@@ -24,5 +25,20 @@ public class ApplicationClassLoader extends URLClassLoader {
 			throw new ClassNotFoundException("net.hycrafthd.headless_minecraft.application package should not be accessed with the application classloader");
 		}
 		return super.loadClass(name, resolve);
+	}
+	
+	private static ClassLoader getPlatformClassLoader() {
+		try {
+			final Method method = ClassLoader.class.getMethod("getPlatformClassLoader");
+			
+			try {
+				return (ClassLoader) method.invoke(null);
+			} catch (Exception ex) {
+				throw new IllegalStateException("Method getPlatformClassLoader could not be invoked", ex);
+			}
+		} catch (NoSuchMethodException ex) {
+			// Okay as the method is only usefull for jdk9+. For jdk8 we return null
+			return null;
+		}
 	}
 }
