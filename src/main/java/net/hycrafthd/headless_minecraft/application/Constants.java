c6a932c9959e065c1547dd9a73b222764f04109c
HyCraftHD <hycrafthd@live.de>
2021-04-20 16:17:40 +0200

    Add constants

---
+++ src/main/java/net/hycrafthd/headless_minecraft/application/Constants.java
@@ -0,0 +1,8 @@
+package net.hycrafthd.headless_minecraft.application;
+
+public class Constants {
+	
+	public static final String CLASSPATH_DIRECTORY = "Application-Classpath-Directory";
+	public static final String CLASSPATH_NAMES = "Application-Classpath-Names";
+	
+}
