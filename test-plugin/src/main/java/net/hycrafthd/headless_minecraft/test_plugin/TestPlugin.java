902ac9b9b11cbe5dead17ae5e19e3216fb455a8f
HyCraftHD <hycrafthd@live.de>
2021-05-01 21:23:05 +0200

    Add basic logger

--- test-plugin/src/main/java/net/hycrafthd/headless_minecraft/test_plugin/TestPlugin.java
+++ test-plugin/src/main/java/net/hycrafthd/headless_minecraft/test_plugin/TestPlugin.java
@@ -1,16 +1,23 @@
 package net.hycrafthd.headless_minecraft.test_plugin;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import net.hycrafthd.headless_minecraft.plugin.HeadlessEventBus;
 import net.hycrafthd.headless_minecraft.plugin.HeadlessPlugin;
 
 public class TestPlugin implements HeadlessPlugin {
 	
+	private static Logger LOGGER = LogManager.getLogger();
+	
 	@Override
 	public void load() {
+		LOGGER.info("Load test plugin");
 	}
 	
 	@Override
 	public void enable() {
+		LOGGER.info("Enable test plugin");
 	}
 	
 	@Override
