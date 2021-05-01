b2eb77f3445e454fbe0d5ef238be06b241c94ef6
HyCraftHD <hycrafthd@live.de>
2021-05-01 20:59:27 +0200

    Restructure project

---
+++ test-plugin/src/main/java/xyz/TestPlugin.java
@@ -0,0 +1,16 @@
+package xyz;
+
+import net.hycrafthd.headless_minecraft.plugin.HeadlessEventBus;
+import net.hycrafthd.headless_minecraft.plugin.HeadlessPlugin;
+
+public class TestPlugin implements HeadlessPlugin {
+	
+	@Override
+	public void load() {
+	}
+	
+	@Override
+	public void register(HeadlessEventBus eventBus) {
+	}
+	
+}
