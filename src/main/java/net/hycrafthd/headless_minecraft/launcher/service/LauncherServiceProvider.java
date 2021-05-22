0769e1c9bc6f76b2a2ca74c8e78cf6d22a701d84
HyCraftHD <hycrafthd@live.de>
2021-05-23 01:52:51 +0200

    Add common project and rename plugin to main plugin

--- src/main/java/net/hycrafthd/headless_minecraft/launcher/service/LauncherServiceProvider.java
+++ src/main/java/net/hycrafthd/headless_minecraft/launcher/service/LauncherServiceProvider.java
@@ -1,12 +1,19 @@
 package net.hycrafthd.headless_minecraft.launcher.service;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.AbstractMap;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 import java.util.function.BiFunction;
@@ -20,7 +27,6 @@ import cpw.mods.modlauncher.api.ITransformer;
 import cpw.mods.modlauncher.api.IncompatibleEnvironmentException;
 import joptsimple.OptionSpec;
 import joptsimple.OptionSpecBuilder;
-import net.hycrafthd.headless_minecraft.launcher.Constants;
 import net.hycrafthd.headless_minecraft.launcher.setup.MinecraftSetup;
 
 public class LauncherServiceProvider implements ITransformationService {
@@ -49,14 +55,21 @@ public class LauncherServiceProvider implements ITransformationService {
 	
 	@Override
 	public void initialize(IEnvironment environment) {
+		// Validate supplied parameters
 		final String version = environment.getProperty(IEnvironment.Keys.VERSION.get()).orElseThrow(() -> new IllegalStateException("Version key must be present"));
 		final Path runDirectory = environment.getProperty(IEnvironment.Keys.GAMEDIR.get()).orElseThrow(() -> new IllegalStateException("Run directory key must be present"));
+		if (authFile == null) {
+			throw new IllegalStateException("Auth file cannot be null");
+		}
+		
+		// Log information
 		LOGGER.debug("Launch headless minecraft version {}", version);
-		LOGGER.debug("The run directory is {}", runDirectory);
+		LOGGER.debug("The run directory is {}", runDirectory.toAbsolutePath());
+		LOGGER.debug("The auth file is {}", authFile.toPath().toAbsolutePath());
 		
 		// Minecraft setup
 		LOGGER.debug("Setup minecraft environment");
-		MinecraftSetup.launch(runDirectory.toFile(), authFile, authenticateType != null, authenticateType);
+		MinecraftSetup.setup(runDirectory.toFile(), authFile, authenticateType != null, authenticateType);
 	}
 	
 	@Override
@@ -66,7 +79,37 @@ public class LauncherServiceProvider implements ITransformationService {
 	@Override
 	public List<Entry<String, Path>> runScan(IEnvironment environment) {
 		beginScanning(environment);
-		return Arrays.asList(new AbstractMap.SimpleImmutableEntry<>("implementation", Paths.get(Constants.DEVELOPMENT_IMPLEMENTATION_BUILD)), new AbstractMap.SimpleImmutableEntry<>("plugin", Paths.get(Constants.DEVELOPMENT_TEST_PLUGIN_BUILD)));
+		
+		try {
+			final URL url = LauncherServiceProvider.class.getResource("/META-INF/MANIFEST.MF");
+			System.out.println(url);
+			
+			URI uri = url.toURI();
+			Map<String, String> env = Collections.emptyMap();
+			FileSystem zipfs = FileSystems.newFileSystem(uri, env);
+			
+			System.out.println(zipfs);
+			System.out.println(zipfs.provider());
+			
+			final Path path = zipfs.getPath("headless_minecraft_implementation-1.16.5-1.0.0-SNAPSHOT.jar");
+			System.out.println(path);
+			
+			return Arrays.asList(new AbstractMap.SimpleImmutableEntry<>("implementation", path));
+			
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		throw new IllegalStateException("XOX");
+		
+		// return Arrays.asList(new AbstractMap.SimpleImmutableEntry<>("implementation",
+		// Paths.get(Constants.DEVELOPMENT_IMPLEMENTATION_BUILD)), new AbstractMap.SimpleImmutableEntry<>("plugin",
+		// Paths.get(Constants.DEVELOPMENT_TEST_PLUGIN_BUILD)));
 	}
 	
 	@Override
