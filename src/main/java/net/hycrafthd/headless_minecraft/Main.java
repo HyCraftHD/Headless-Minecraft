d40f9f2dee836ca33e992defd659aabd17a7482f
HyCraftHD <hycrafthd@live.de>
2022-01-27 22:04:03 +0100

    Cleanup build gradle a bit with runs and add other main class for testing

---
+++ src/main/java/net/hycrafthd/headless_minecraft/Main.java
@@ -0,0 +1,10 @@
+package net.hycrafthd.headless_minecraft;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("HELLO from main");
+		net.minecraft.client.main.Main.main(args);
+	}
+
+}
