0bbaef2c1da755bfe013f031a4472838a1bdcf6a
HyCraftHD <hycrafthd@live.de>
2022-01-27 22:19:05 +0100

    Test at

--- src/main/java/net/hycrafthd/headless_minecraft/Main.java
+++ src/main/java/net/hycrafthd/headless_minecraft/Main.java
@@ -3,7 +3,7 @@ package net.hycrafthd.headless_minecraft;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("HELLO from main");
+		net.minecraft.client.main.Main.LOGGER.info("HELLO from main");
 		net.minecraft.client.main.Main.main(args);
 	}
 
