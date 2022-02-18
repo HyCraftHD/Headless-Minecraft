9702eafb8fee75331cc30ca898ddf1f5bc38adf2
Luc_ZZ <lucas.weiss1999@web.de>
2022-02-18 20:46:26 +0100

    Fixed Stuff

--- src/main/java/net/hycrafthd/headless_minecraft/event_system/events/PlayerTickEvent.java
+++ src/main/java/net/hycrafthd/headless_minecraft/event_system/events/PlayerTickEvent.java
@@ -3,28 +3,28 @@ package net.hycrafthd.headless_minecraft.event_system.events;
 import net.hycrafthd.headless_minecraft.event_system.Event;
 
 public abstract class PlayerTickEvent implements Event {
-	
-	private HeadlessPlayer player;
-	
-	protected PlayerTickEvent(HeadlessPlayer player) {
-		this.player = player;
-	}
-	
-	public HeadlessPlayer getPlayer() {
-		return player;
-	}
-	
-	public static class Pre extends PlayerTickEvent {
-		
-		public Pre(HeadlessPlayer player) {
-			super(player);
-		}
-	}
-	
-	public static class Post extends PlayerTickEvent {
-		
-		public Post(HeadlessPlayer player) {
-			super(player);
-		}
-	}
+
+//	private HeadlessPlayer player;
+//	
+//	protected PlayerTickEvent(HeadlessPlayer player) {
+//		this.player = player;
+//	}
+//	
+//	public HeadlessPlayer getPlayer() {
+//		return player;
+//	}
+//	
+//	public static class Pre extends PlayerTickEvent {
+//		
+//		public Pre(HeadlessPlayer player) {
+//			super(player);
+//		}
+//	}
+//	
+//	public static class Post extends PlayerTickEvent {
+//		
+//		public Post(HeadlessPlayer player) {
+//			super(player);
+//		}
+//	}
 }
