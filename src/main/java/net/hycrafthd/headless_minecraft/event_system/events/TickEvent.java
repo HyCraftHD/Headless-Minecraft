5da31ab6a0b099cc1bbf58f73822b01c226ab54e
Luc_ZZ <lucas.weiss1999@web.de>
2022-02-21 21:10:15 +0100

    TickEvent

--- src/main/java/net/hycrafthd/headless_minecraft/event_system/events/TickEvent.java
+++ src/main/java/net/hycrafthd/headless_minecraft/event_system/events/TickEvent.java
@@ -1,9 +1,7 @@
 package net.hycrafthd.headless_minecraft.event_system.events;
 
-import net.hycrafthd.headless_minecraft.event_system.Event;
+public class TickEvent {
 
-public class TickEvent implements Event {
-	
 	public TickEvent() {
 	}
 	
