2fdb795e3b99fd4476454bfb1151785d228b9766
Luc_ZZ <lucas.weiss1999@web.de>
2022-02-27 22:17:30 +0100

    Tick Event working

--- headless-minecraft-impl/src/main/java/net/hycrafthd/headless_minecraft/Main.java
+++ headless-minecraft-impl/src/main/java/net/hycrafthd/headless_minecraft/Main.java
@@ -3,34 +3,45 @@ package net.hycrafthd.headless_minecraft;
 import net.hycrafthd.headless_minecraft.event_system.EventHandler;
 import net.hycrafthd.headless_minecraft.event_system.EventManager;
 import net.hycrafthd.headless_minecraft.event_system.commands.CommandExecution;
-import net.hycrafthd.headless_minecraft.event_system.commands.CommandRegistry;
-import net.hycrafthd.headless_minecraft.event_system.events.ChatTellEvent;
-import net.hycrafthd.headless_minecraft.event_system.events.RawServerChatMessageEvent;
+import net.hycrafthd.headless_minecraft.event_system.events.PlayerTickEvent;
+import net.hycrafthd.headless_minecraft.event_system.events.TickEvent;
+import net.minecraft.client.Minecraft;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.EntityHitResult;
+import net.minecraft.world.phys.HitResult;
 
 public class Main {
 
 	public static void main(String[] args) {
 		net.minecraft.client.main.Main.LOGGER.info("HELLO from main");
-		
+		EventManager.registerListener(new CommandExecution());
 		new Main();
-		
 		net.minecraft.client.main.Main.main(args);
 	}
-
+	
 	public Main() {
 		EventManager.registerListener(this);
-		EventManager.registerListener(new CommandExecution());
-		CommandRegistry.registerCommand("test", (command, args, player) -> { System.out.println("Command!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");}, "t");
 	}
 	
 	@EventHandler
-	public void event(RawServerChatMessageEvent event) {
-		//System.out.println(event);
+	public void tick(TickEvent event) {
+		//System.out.println("Tick");
 	}
 	
 	@EventHandler
-	public void event2(ChatTellEvent event) {
-		//System.out.println(event);
+	public void playerTick(PlayerTickEvent.Pre event) {
+		//System.out.println("PlayerTickPre");
+		if(Minecraft.getInstance().hitResult instanceof BlockHitResult result) {
+			System.out.println(event.getPlayer().getLevel().getBlockState(result.getBlockPos()));
+			
+		} else if(Minecraft.getInstance().hitResult instanceof EntityHitResult result) {
+			System.out.println(result.getEntity());
+		}
 	}
-
+	
+	@EventHandler
+	public void playerTick(PlayerTickEvent.Post event) {
+		//System.out.println("PlayerTickPost");
+	}
+	
 }
