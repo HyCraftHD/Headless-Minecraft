48a4560789a6effb5010c279491b127c34a3ab8c
HyCraftHD <hycrafthd@live.de>
2022-03-12 19:07:10 +0100

    Fix left click interaction (wip)

--- headless-minecraft-impl/src/main/java/net/hycrafthd/headless_minecraft/Main.java
+++ headless-minecraft-impl/src/main/java/net/hycrafthd/headless_minecraft/Main.java
@@ -1,17 +1,21 @@
 package net.hycrafthd.headless_minecraft;
 
+import java.lang.reflect.Field;
+
 import net.hycrafthd.headless_minecraft.event_system.EventHandler;
 import net.hycrafthd.headless_minecraft.event_system.EventManager;
 import net.hycrafthd.headless_minecraft.event_system.commands.CommandExecution;
+import net.hycrafthd.headless_minecraft.event_system.commands.CommandRegistry;
 import net.hycrafthd.headless_minecraft.event_system.events.PlayerTickEvent;
 import net.hycrafthd.headless_minecraft.event_system.events.TickEvent;
 import net.minecraft.client.Minecraft;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.EntityHitResult;
-import net.minecraft.world.phys.HitResult;
 
 public class Main {
-
+	
+	public static boolean left_click = false;
+	
 	public static void main(String[] args) {
 		net.minecraft.client.main.Main.LOGGER.info("HELLO from main");
 		EventManager.registerListener(new CommandExecution());
@@ -21,27 +25,40 @@ public class Main {
 	
 	public Main() {
 		EventManager.registerListener(this);
+		
+		CommandRegistry.registerCommand("break", (command, args, sender) -> {
+			Field field;
+			try {
+				field = Minecraft.class.getDeclaredField("missTime");
+				field.setAccessible(true);
+				System.out.println("Misstime: " + field.get(Minecraft.getInstance()));
+			} catch (Exception e) {
+			}
+			Minecraft.getInstance().startAttack();
+			left_click = true;
+			System.out.println("Break");
+		});
 	}
 	
 	@EventHandler
 	public void tick(TickEvent event) {
-		//System.out.println("Tick");
+		// System.out.println("Tick");
 	}
 	
 	@EventHandler
 	public void playerTick(PlayerTickEvent.Pre event) {
-		//System.out.println("PlayerTickPre");
-		if(Minecraft.getInstance().hitResult instanceof BlockHitResult result) {
-			System.out.println(event.getPlayer().getLevel().getBlockState(result.getBlockPos()));
+		// System.out.println("PlayerTickPre");
+		if (Minecraft.getInstance().hitResult instanceof BlockHitResult result) {
+			// System.out.println(event.getPlayer().getLevel().getBlockState(result.getBlockPos()));
 			
-		} else if(Minecraft.getInstance().hitResult instanceof EntityHitResult result) {
-			System.out.println(result.getEntity());
+		} else if (Minecraft.getInstance().hitResult instanceof EntityHitResult result) {
+			// System.out.println(result.getEntity());
 		}
 	}
 	
 	@EventHandler
 	public void playerTick(PlayerTickEvent.Post event) {
-		//System.out.println("PlayerTickPost");
+		// System.out.println("PlayerTickPost");
 	}
 	
 }
--- headless-minecraft-impl/src/main/java/net/hycrafthd/headless_minecraft/event_system/commands/CommandRegistry.java
+++ headless-minecraft-impl/src/main/java/net/hycrafthd/headless_minecraft/event_system/commands/CommandRegistry.java
@@ -3,34 +3,33 @@ package net.hycrafthd.headless_minecraft.event_system.commands;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 import java.util.UUID;
-import java.util.stream.Stream;
 
 import net.hycrafthd.headless_minecraft.event_system.CommandRegisterException;
 
 public class CommandRegistry {
-
+	
 	private static HashMap<String, CommandAction> commands = new HashMap<>();
-
+	
 	private static final ArrayList<UUID> allowedPlayers = new ArrayList<>();
-
+	
 	private static HashMap<String, String> aliasForCommand = new HashMap<String, String>();
-
+	
 	static {
 		allowedPlayers.add(UUID.fromString("1e26ade8-8288-4963-87a5-b478f08c0633"));
+		allowedPlayers.add(UUID.fromString("d9202ce0-f6c1-4dc1-9341-2d9091764808"));
 	}
-
+	
 	private CommandRegistry() {
 	}
-
+	
 	public static void registerCommand(String command, CommandAction action) {
 		if (commands.containsKey(command)) {
 			throw new CommandRegisterException("Command: " + command + " already registered!");
 		}
 		commands.put(command, action);
 	}
-
+	
 	public static void registerCommand(String command, CommandAction action, String... alias) {
 		if (commands.containsKey(command)) {
 			throw new CommandRegisterException("Command: " + command + " already registered!");
@@ -39,22 +38,22 @@ public class CommandRegistry {
 			throw new CommandRegisterException("Alias not found!");
 		}
 		commands.put(command, action);
-		for(String a : alias) {
+		for (String a : alias) {
 			aliasForCommand.put(a, command);
 		}
 	}
-
+	
 	public static void executeCommand(String message, UUID sender) {
-		if(!allowedPlayers.contains(sender)) {
+		if (!allowedPlayers.contains(sender)) {
 			return;
 		}
 		String[] t = message.split(" ");
 		String command = t[0];
 		String[] args = Arrays.copyOfRange(t, 1, t.length);
 		if (commands.containsKey(command)) {
-			commands.get(command).action(command, args, sender); //TODO
+			commands.get(command).action(command, args, sender); // TODO
 		}
-		if(aliasForCommand.containsKey(command)) {
+		if (aliasForCommand.containsKey(command)) {
 			commands.get(aliasForCommand.get(command)).action(command, args, sender);
 		}
 	}
